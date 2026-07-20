aebacffcf0be2139fd0a12c78719f2ec3163ca6c
Binoy Babu K <140301330+BinoyBabu10@users.noreply.github.com>
2026-07-20 20:34:24 +0530

    Time: 1 ms (99.55%), Space: 42.4 MB (96.76%) - LeetHub

---
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxScore(String s) {
+        int right=0;
+        for(char c:s.toCharArray()){
+
+            if(c=='1'){
+            right++;
+            }
+        }
+        int l=0;
+        int max=0;
+        for(int i=0;i<s.length()-1;i++){
+            if(s.charAt(i)=='0'){
+                l++;
+            }
+            else{
+                right--;
+            }
+            max=Math.max(max,l+right);
+        }
+        return max;
+
+    }
+}
