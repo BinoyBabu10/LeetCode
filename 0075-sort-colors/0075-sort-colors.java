59ce8da7031a27b8ba8b97b3708adf736796f5ef
Binoy Babu K <140301330+BinoyBabu10@users.noreply.github.com>
2026-08-02 10:14:36 +0530

    Time: 0 ms (100%), Space: 43.6 MB (25.69%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,8 +1,7 @@
 class Solution {
     public void sortColors(int[] nums) {
         int n=nums.length;
-        int low=0;
-        int mid=0;
+        int low=0,mid=0;
         int high=n-1;
         while(mid<=high){
             if(nums[mid]==0){
@@ -16,9 +15,9 @@ class Solution {
                 mid++;
             }
             else{
-                int temp=nums[mid];
-                nums[mid]=nums[high];
-                nums[high]=temp;
+                int temp=nums[high];
+                nums[high]=nums[mid];
+                nums[mid]=temp;
                 high--;
             }
         }
