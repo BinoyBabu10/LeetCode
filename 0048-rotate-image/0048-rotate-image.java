6da0198801de3afa44a463141da424f7b73d3ae3
Binoy Babu K <140301330+BinoyBabu10@users.noreply.github.com>
2026-08-18 19:17:30 +0530

    Time: 0 ms (100%), Space: 44.1 MB (16.69%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,7 +1,7 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n=matrix.length;
-        //Transpose
+        //transpose
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 int temp=matrix[i][j];
@@ -13,7 +13,7 @@ class Solution {
         for(int i=0;i<n;i++){
             int l=0;
             int r=n-1;
-            while(l<r){
+            while(l<=r){
                 int temp=matrix[i][l];
                 matrix[i][l]=matrix[i][r];
                 matrix[i][r]=temp;
