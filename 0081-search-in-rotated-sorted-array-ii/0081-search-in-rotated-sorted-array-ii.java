75e0d37ed6ba684041aea3b6c93d8729d6f9abba
Binoy Babu K <140301330+BinoyBabu10@users.noreply.github.com>
2026-08-31 21:15:46 +0530

    Time: 0 ms (100%), Space: 45 MB (47.4%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -3,16 +3,16 @@ class Solution {
         int l=0;
         int r=nums.length-1;
         while(l<=r){
-            int mid=(l+r)/2;
+            int mid=(l+r)>>1;
             if(nums[mid]==target){
                 return true;
             }
-            if(nums[mid]== nums[l]&& nums[mid]== nums[r]){
+            if(nums[l]==nums[mid] && nums[mid]==nums[r]){
                 l++;
                 r--;
             }
             else if(nums[l]<=nums[mid]){
-                if(nums[l]<=target && nums[mid]>target ){
+                if(nums[l]<=target && nums[mid]>target){
                     r=mid-1;
                 }
                 else{
@@ -20,13 +20,12 @@ class Solution {
                 }
             }
             else{
-                if(nums[mid]<=target && nums[r]>=target){
+                if(nums[mid]<target && target<=nums[r]){
                     l=mid+1;
                 }
                 else{
                     r=mid-1;
                 }
-
             }
         }
         return false;
