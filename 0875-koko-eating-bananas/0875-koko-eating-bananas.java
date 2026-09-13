bda253778507cf0846d6ee8fd7cd8a096dd6588b
Binoy Babu K <140301330+BinoyBabu10@users.noreply.github.com>
2026-09-13 13:59:17 +0530

    Time: 6 ms (99.77%), Space: 47.9 MB (42.69%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,27 +1,27 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int l=1;
-        int high=0;
-        for(int p:piles){
-            high=Math.max(high,p);
+       int l=1;
+       int high=0;
+       for(int n:piles){
+        high=Math.max(n,high);
+       }
+       while(l<high){
+        int mid=(l+high)>>1;
+        if(caneat(piles,h,mid)){
+            high=mid;
         }
-        while(l<high){
-            int mid=l+(high-l)/2;
-            if(caneat(piles,h,mid)){
-                high=mid;
-            }
-            else{
-                l=mid+1;
-            }
+        else{
+            l=mid+1;
         }
-        return l;
+       }
+       return l;
     }
-
     public boolean caneat(int[] piles,int h,int speed){
         int hour=0;
-        for(int pile:piles){
-            hour+=(pile+speed-1)/speed;
+        for(int p:piles){
+            hour+=(p+speed-1)/speed;
         }
         return hour<=h;
-    }    
+    }
+
 }
