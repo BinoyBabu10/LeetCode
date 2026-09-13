2c31431847b600cd57de6213e4de20d247b7f4d8
Binoy Babu K <140301330+BinoyBabu10@users.noreply.github.com>
2026-09-13 10:40:21 +0530

    Time: 0 ms (100%), Space: 47.8 MB (97.62%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,9 +1,9 @@
 class Solution {
     public int search(int[] nums, int target) {
         int low=0;
-        int high=nums.length-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
+        int right=nums.length-1;
+        while(low<=right){
+            int mid=(low+right)>>1;
             if(nums[mid]==target){
                 return mid;
             }
@@ -11,7 +11,7 @@ class Solution {
                 low=mid+1;
             }
             else{
-                high=mid-1;
+                right=mid-1;
             }
         }
         return -1;
